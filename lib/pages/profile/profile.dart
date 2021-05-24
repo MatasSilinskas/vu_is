5b5ef5373b746f56abd2b7e774b869c4fdcd47e9
Matas Šilinskas <matas.silinskas@iterato.lt>
2021-05-25 00:32:08 +0300

    Move student information to separate file

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:vu_is/localization/keys.dart';
+import 'package:vu_is/pages/profile/widgets/student_information.dart';
 import 'package:vu_is/shared/models/user.dart';
 import 'package:vu_is/shared/widgets/vu_tab_controller.dart';
-import 'package:intl/intl.dart';
 
 class ProfilePage extends StatefulWidget {
   final User user;
@@ -29,65 +29,7 @@ class _ProfilePageState extends State<ProfilePage> {
         Tab(text: translate(Keys.Tabs_Userinformation)),
       ],
       bodies: [
-        Scaffold(
-            body: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Text(
-                translate(Keys.Student_Title, args: {'studentNr': user.studentNumber}),
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(user.study.getExtendedType()),
-              Text(user.study.university.faculty.getName()),
-              Text(user.study.getDescription()),
-              Text(user.study.getProgressDescription()),
-              SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(user.study.getFinanceInformation()),
-              ),
-              SizedBox(height: 20),
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(child: Text(translate(Keys.Studies_Start))),
-                  Expanded(child: Text(user.study.start.year.toString())),
-                ],
-              ),
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(child: Text(translate(Keys.Studies_Plannedend))),
-                  Expanded(child: Text(DateFormat('yyyy-MM-dd').format(user.study.plannedEnd))),
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(child: Text(translate(Keys.Studies_Language))),
-                  Expanded(child: Text(translate(user.study.getLanguage()))),
-                ],
-              ),
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(child: Text(translate(Keys.Student_Acceptedgrade))),
-                  Expanded(child: Text(user.study.acceptedGrade.toString())),
-                ],
-              ),
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(child: Text(translate(Keys.Student_Status_Label))),
-                  Expanded(child: Text(user.getStatus())),
-                ],
-              ),
-            ],
-          ),
-        )),
+        StudentInformation(user: user),
         Scaffold(),
       ],
     );
---
+++ lib/pages/profile/widgets/student_information.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+import 'package:vu_is/localization/keys.dart';
+import 'package:vu_is/shared/models/user.dart';
+import 'package:intl/intl.dart';
+
+class StudentInformation extends Scaffold {
+  final User user;
+
+  StudentInformation({required this.user});
+
+  @override
+  Widget? get body => Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text(
+              translate(Keys.Student_Title, args: {'studentNr': user.studentNumber}),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(user.study.getExtendedType()),
+            Text(user.study.university.faculty.getName()),
+            Text(user.study.getDescription()),
+            Text(user.study.getProgressDescription()),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(user.study.getFinanceInformation()),
+            ),
+            SizedBox(height: 20),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(child: Text(translate(Keys.Studies_Start))),
+                Expanded(child: Text(user.study.start.year.toString())),
+              ],
+            ),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(child: Text(translate(Keys.Studies_Plannedend))),
+                Expanded(child: Text(DateFormat('yyyy-MM-dd').format(user.study.plannedEnd))),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(child: Text(translate(Keys.Studies_Language))),
+                Expanded(child: Text(translate(user.study.getLanguage()))),
+              ],
+            ),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(child: Text(translate(Keys.Student_Acceptedgrade))),
+                Expanded(child: Text(user.study.acceptedGrade.toString())),
+              ],
+            ),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(child: Text(translate(Keys.Student_Status_Label))),
+                Expanded(child: Text(user.getStatus())),
+              ],
+            ),
+          ],
+        ),
+      );
+}
